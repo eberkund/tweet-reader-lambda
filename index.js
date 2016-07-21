1c9425dcaefaf0c772e5b7cbe8bd8cc82b68b0ae
Erik Berkun-Drevnig <erik.b22@gmail.com>
2016-07-21 00:49:50 -0400

    Fixed and working

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-var Twitter = require('twitter');
-var AWS = require('aws-sdk');
+'use strict'
 
-require('dotenv').config();
+const Twitter = require('twitter')
+const AWS = require('aws-sdk')
 
-exports.handler = function(event, context, callback) {
-    var twitter = new Twitter({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    });
+require('dotenv').config()
 
-    var params = {
-        screen_name: 'campingbabble'
-    };
+exports.handler = function (event, context, callback) {
+  let twitter = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
 
-    twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-            upload(tweets);
-        }
-        else {
-            console.log(error, error.stack);
-        }
-    });
-};
+  let params = {
+    screen_name: process.env.TWITTER_SCREEN_NAME
+  }
 
-function upload(data) {
-    var s3 = new AWS.S3();
+  twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+      upload(tweets)
+    } else {
+      console.log(error, error.stack)
+    }
+  })
+}
 
-    params = {
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: process.env.AWS_S3_KEY,
-        Body: JSON.stringify(data)
-    };
+function upload (data) {
+  let s3 = new AWS.S3()
+  data.length = 2
 
-    s3.upload(params, function(error, data) {
-        if (error) {
-            console.log(err, err.stack);
-        }
-    });
+  let params = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: process.env.AWS_S3_KEY,
+    Body: JSON.stringify(data),
+    ContentType: 'application/json'
+  }
+
+  s3.upload(params, function (error, data) {
+    if (error) {
+      console.log(error, error.stack)
+    }
+  })
 }
